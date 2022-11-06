432bbc6263422c39e20112098c0b214a20f01b2a
lxfxy <3357038237@qq.com>
2022-11-06 15:54:39 +0800

    🎉 initial

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        // AutoImport({
+        //     imports: [
+        //         "vue",
+        //         {
+        //             "naive-ui": [
+        //                 "useDialog",
+        //                 "useMessage",
+        //                 "useNotification",
+        //                 "useLoadingBar",
+        //             ],
+        //         },
+        //     ],
+        // }),
+        // Components({
+        //     resolvers: [NaiveUiResolver()],
+        // }),
+    ],
+});
